7a319ef9839aeac5b9248b3b2912199f9199c5ee
wafaankoush99 <wafadirawe@gmail.com>
2021-04-13 18:39:02 +0300

    lab11

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = express();
 
 const superagent = require('superagent');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.use(cors());
 
@@ -19,48 +19,43 @@ server.use(express.urlencoded({ extended: true }));
 
 server.set('view engine', 'ejs');
 
-// server.use(express.static('public'));
 
 server.get('/', (req, res) => {
     res.render('./pages/index');
 });
 
+server.get('/searches/new', (req, res) => {
+    res.render('./pages/searches/new');
+});
+
+
 server.post('/searches', (req, res) => {
-    // console.log(req.query);
-    let searchBy = req.body.words;
-    let words = req.body.searchBy;
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${searchBy}+${words}`;
+    let search = req.body.search;
+    let term = req.body.intitle;
+    let url = `https://www.googleapis.com/books/v1/volumes?q=+${term}:${search}`;
     superagent.get(url)
         .then(data => {
             let books = data.body.items.map(book => new Book(book));
             console.log(books);
-            res.render('./pages/searches/show', { books: books });
+            res.render('pages/searches/show', { books: books });
         });
 });
-
 
 function Book(data) {
     this.title = data.volumeInfo.title;
     this.author = data.volumeInfo.authors;
     this.description = data.volumeInfo.description;
-    this.image = data.volumeInfo.imageLinks.thumbnail;
+    this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
 
-// server.get( '*',( req,res )=>{
-//     let errorObject = {
-//       status: 500,
-//       responseText: 'Sorry, something went wrong'
-//     };
-//     res.status( 500 ).send( errorObject );
-//   } );
 
 
-  server.get('*',(req,res)=>{
+server.get('*',(req,res)=>{
     let errorObject = {
         status: 500,
         responseText: 'Sorry, something went wrong'
-      };
-      res.render('./pages/error',{errorObject});
-})
+    };
+    res.render('./pages/error',{errorObject});
+});
 
 server.listen(PORT, () => console.log('Up on port', PORT));
